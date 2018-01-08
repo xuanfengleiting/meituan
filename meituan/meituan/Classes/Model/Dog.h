0aae169780d8d6d4c6ce6c1ec39d723dc6c0537e
XF <qinbahua@163.com>
2018-01-08 22:29:38 +0800

    添加了Dog类。

---
+++ meituan/meituan/Classes/Model/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  meituan
+//
+//  Created by jnl on 2018/1/8.
+//  Copyright © 2018年 newenergy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ meituan/meituan/Classes/Model/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  meituan
+//
+//  Created by jnl on 2018/1/8.
+//  Copyright © 2018年 newenergy. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
